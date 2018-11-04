6c20b3a32c5874e23e064a72c861aad7de9946cb
Veg_chic <h_foryou@163.com>
2018-11-04 21:20:55 +0800

    实现本地文件传输

--- client.py
+++ client.py
@@ -1,4 +1,24 @@
 import socket
+from utils import logger, operation
+
+class Client:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def handle(self, filename, operation = operation.Iget):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.sendto(bytes(filename, 'utf-8'), (self.host, self.port))
+        f = open('receive', 'wb')
+        while True:
+            data = s.recv(1024)
+            f.write(data)
+            if len(data) < 1024:
+                break
+        f.close()
+        s.close()
 
 if __name__ == '__main__':
-    pass
+    HOST, PORT = 'localhost', 9999
+    client = Client(HOST, PORT)
+    client.handle('test.jpg')
--- server.py
+++ server.py
@@ -1,16 +1,34 @@
 import socketserver
-import socket
 import threading
-from utils import logger
+from utils import logger, operation
 
 
 class UDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
+    """
+    use for mixin
+    """
     pass
 
 
 class ServerHandler(socketserver.BaseRequestHandler):
-  def handle(self):
-    pass
+    """
+    handle udp requests
+    """
+
+    def setup(self):
+        host, port = self.client_address
+        self.logger = logger(threading.current_thread().name)
+        self.logger.log('Recieve request from {}:{}'.format(host, port))
+
+    def handle(self):
+        filename = str(self.request[0].strip(), 'utf-8')
+        f = open(filename, 'rb')
+        l = f.read(1024)
+        socket = self.request[1]
+        while l:
+            socket.sendto(l, self.client_address)
+            l = f.read(1024)
+        self.logger.log('Send {} done'.format(filename))
 
 
 if __name__ == '__main__':
@@ -22,4 +40,4 @@ if __name__ == '__main__':
 
     server_thread.start()
 
-    logger('Listen on {}:{}'.format(HOST, PORT))
+    logger('Server: ').log('Listen on {}:{}'.format(HOST, PORT))

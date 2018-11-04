d76959ec4db030ed6719bc8645998c4a6f342018
Veg_chic <h_foryou@163.com>
2018-11-04 23:18:45 +0800

    实现 sender 简单的  send

--- sender.py
+++ sender.py
@@ -1,2 +1,15 @@
 class Sender:
-    pass
+    
+    def __init__(self, host, port, filename):
+        self.address = (host, port)
+        self.f = open(filename, 'rb')
+        self.filename = filename
+        self.done = False
+
+    def send(self, socket):
+        data = self.f.read(1024)
+        if data:
+            socket.sendto(data, self.address)
+        else:
+            self.f.close()
+            self.done = True

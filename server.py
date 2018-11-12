944a00e8df846a4ef6c20ca6cd1ce559ca593784
Veg_chic <h_foryou@163.com>
2018-11-12 22:34:42 +0800

    暂时移除线程池，增加operation 判断

--- server.py
+++ server.py
@@ -4,18 +4,7 @@ from utils import logger, operation
 from sender import Sender
 from receiver import Reciever
 
-senders = {}
-receivers = {}
-
-sender_lock = threading.Lock()
-receiver_lock = threading.Lock()
-
-class UDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
-    """
-    use for mixin
-    """
-    pass
-
+pool = {}
 
 class ServerHandler(socketserver.BaseRequestHandler):
     """
@@ -25,29 +14,30 @@ class ServerHandler(socketserver.BaseRequestHandler):
     def setup(self):
         host, port = self.client_address
         self.logger = logger('client: {}:{}'.format(host, port))
-        self.logger.log('Recieve request from {}:{}'.format(host, port))
 
     def handle(self):
         host, port = self.client_address
-        if senders.get(self.client_address) == None:
-            filename = str(self.request[0].strip(), 'utf-8')
-            sender_lock.acquire()
-            senders[self.client_address] = Sender(host, port, filename)
-            sender_lock.release()
+        if pool.get(self.client_address) == None:
+            opt, filename = str(self.request[0].strip(), 'utf-8').split(' ')
+            if opt == operation.Iget:
+                pool[self.client_address] = Sender(host, port, filename)
+            else:
+                pool[self.client_address] = Reciever(host, port, filename)
 
-        sender = senders[self.client_address]
-        sender.send(self.request[1])
-        if sender.done:
-            self.logger.log('Send {} done'.format(sender.filename))
+        worker = pool[self.client_address]
+        worker.work(self.request[1])
+        if worker.done:
+            pool.pop(self.client_address)
+            self.logger.log('{} {} done'.format(worker.opt, worker.filename))
+        else:
+            self.logger.log('{}: {} / {} bytes'.format(worker.filename, worker.f.tell(), worker.statinfo.st_size))
 
 
 if __name__ == '__main__':
     HOST, PORT = 'localhost', 9999
 
-    server = UDPServer((HOST, PORT), ServerHandler)
-
-    server_thread = threading.Thread(target=server.serve_forever)
-
-    server_thread.start()
+    server = socketserver.UDPServer((HOST, PORT), ServerHandler)
 
     logger('Server: ').log('Listen on {}:{}'.format(HOST, PORT))
+    
+    server.serve_forever()

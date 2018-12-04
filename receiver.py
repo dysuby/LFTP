a85a37baa3751630634f3aaa008ea0eaeb80bd62
Veg_chic <h_foryou@163.com>
2018-12-04 13:10:24 +0800

    调整 ACK 逻辑 (接上

--- receiver.py
+++ receiver.py
@@ -8,7 +8,7 @@ from utils import Logger, Constant, PACK, Field
 
 
 class Reciever:
-    def __init__(self, addr, port, remotePath, localPath, throw_rate = 0):
+    def __init__(self, addr, port, remotePath, localPath, throw_rate=0):
         self.sender_addr = addr
         self.port = port
         self.f = open(localPath, 'wb')
@@ -34,19 +34,21 @@ class Reciever:
         sc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sc.bind(('localhost', self.port))
         kw = {Field.FILE_NAME: self.remotePath, Field.ACK: ACK,
-              Field.EOF: self.done, Field.RWND: self.ws, Field.PORT: self.port}
+              Field.RWND: self.ws, Field.PORT: self.port}
         while True:
             empty = PACK.serialize(b'', kw)
 
             self.logger.log('Sending ACK: {}'.format(kw[Field.ACK]))
             sc.sendto(empty, self.sender_addr)
             seq = sc.recv(Constant.MSS + Field.HEADER_LEN)
-            
+
             if random.random() >= self.throw_rate:   # 随机丢包
                 rkw, data = PACK.deserialize(seq)
-                self.logger.log('receive SEQ: {} expected SEQ: {}'.format(rkw[Field.SEQ], ACK + 1))
+                self.logger.log('receive SEQ: {} expected SEQ: {}'.format(
+                    rkw[Field.SEQ], ACK + 1))
                 if rkw[Field.SEQ] == ACK + 1 and self.putData(data):
                     ACK += 1
+                    kw[Field.ACK] = rkw[Field.SEQ]
                     self.logger.log('Correct SEQ: {}'.format(ACK))
                     if ACK == rkw[Field.SEQ_NUM] + 1:
                         self.done = True
@@ -55,8 +57,7 @@ class Reciever:
                     self.logger.log('Unexpected SEQ {}'.format(rkw[Field.SEQ]))
                 else:
                     self.logger.log('Full Queue')
-                            
-                kw[Field.ACK] = rkw[Field.SEQ]
+
                 kw[Field.RWND] = self.ws - len(self.buffer)
             yield
         sc.close()
